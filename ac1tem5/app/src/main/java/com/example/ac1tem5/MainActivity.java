b26eafb22cfd3d9352850625fe69e078a1cf2a0a
Ines <icg0024@alu.medac.es>
2025-11-10 17:02:30 +0100

    correo y contraseña

--- ac1tem5/app/src/main/java/com/example/ac1tem5/MainActivity.java
+++ ac1tem5/app/src/main/java/com/example/ac1tem5/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ac1tem5;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText tCorreo;
+    private EditText tContraseña;
+    private Button bContinuar;
+    private Switch sRecordar;
+    private TextView tvMensaje;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,28 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        tCorreo = findViewById(R.id.etCorreo);
+        tContraseña = findViewById(R.id.etContraseña);
+        bContinuar = findViewById(R.id.bContinuar);
+        sRecordar = findViewById(R.id.sRecordar);
+        tvMensaje = findViewById(R.id.tvMensaje);
+
+
+        bContinuar.setOnClickListener(v -> {
+            String correo = tCorreo.getText().toString();
+            String contraseña = tContraseña.getText().toString();
+
+            if(correo.equals("ines@correo.com") && contraseña.equals("limon")){
+                tvMensaje.setText("usuario y contraseña correctos");
+            }
+            else{
+                tvMensaje.setText("usuario y/o contraseña incorrectos");
+
+            }
+
+
         });
     }
 }
